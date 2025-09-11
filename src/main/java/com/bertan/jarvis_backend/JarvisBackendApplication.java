9dd1a1c2e8fd7c2ab5d03c25ab6bb28a40234cd8
rogerbertan <roger.bertan@gmail.com>
2025-09-10 22:11:08 -0300

    feat: iniciar estrutura do Spring Boot

---
+++ src/main/java/com/bertan/jarvis_backend/JarvisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bertan.jarvis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarvisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bertan/jarvis_backend/JarvisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertan.jarvis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
