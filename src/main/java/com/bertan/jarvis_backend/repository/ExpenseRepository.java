5434140549dd0357839a55ee8df79170d6c82612
Roger Bertan <roger.bertan@gmail.com>
2025-12-04 22:01:36 -0300

    feat: implement repository classes

---
+++ src/main/java/com/bertan/jarvis_backend/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.bertan.jarvis_backend.repository;
+
+import com.bertan.jarvis_backend.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository <Expense, Integer> {
+}
